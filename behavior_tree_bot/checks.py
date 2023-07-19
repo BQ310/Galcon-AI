8ea56c78ee96bf5954ddde98d0f2105fc5999d91
bmquach <bmquach@ucsc.edu>
2023-07-18 21:36:31 -0700

    if enemy attack

--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -5,3 +5,7 @@ def if_enemy_spread(state):
     if len(spreaders) > 0:
         return True
     return False
+
+def if_enemy_attack(state):
+    target_id = [planet.ID for planet in state.my_planets()]
+    attackers = [fleet for fleet in state.enemy_fleets() if fleet.destination_planet in target_id]
